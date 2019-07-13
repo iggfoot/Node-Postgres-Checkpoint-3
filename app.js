967649f580318e2856a168ad3e5391238bc1790b
iggfoot <ijneville19@gmail.com>
2019-07-12 23:30:02 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -9,6 +9,217 @@ app.get('/', (req, res) => {
     res.send('Hello Ian!');
 });
 
+/* Drill 1: Create a route handler function on the path /sum that accepts two query
+   parameters named a and b and find the sum of the two values. Return a string in 
+   the format "The sum of a and b is c". Note that query parameters are always strings
+   so some thought should be given to converting them to numbers.*/
+app.get('/sum', (req, res) => {
+    const {a, b} = req.query;
+  
+    // Validation - a and b are required and should be numbers
+    if(!a) {
+      return res
+            .status(400)
+            .send('a is required');
+    }
+  
+    if(!b) {
+      return res
+            .status(400)
+            .send('b is required');
+    }
+  
+    const numA = parseFloat(a);
+    const numB = parseFloat(b);
+  
+    if(Number.isNaN(numA)) {
+      return res
+            .status(400)
+            .send('a must be a number');
+    }
+  
+    if(Number.isNaN(numB)) {
+      return res
+            .status(400)
+            .send('b must be a number');
+    }
+  
+    // validation passed so perform the task
+    const c = numA + numB;
+  
+    // format the response string
+    const responseString = `The sum of ${numA} and ${numB} is ${c}`;
+  
+    // send the response
+    res
+      .status(200)
+      .send(responseString);
+  });
+  
+  /* Drill 2: Create an endpoint /cipher. The handler function should accept a query parameter 
+     named text and one named shift. Encrypt the text using a simple shift cipher also known 
+     as a Caesar Cipher. It is a simple substitution cipher where each letter is shifted a 
+     certain number of places down the alphabet. So if the shift was 1 then A would be 
+     replaced by B, and B would be replaced by C and C would be replaced by D and so on
+      until finally Z would be replaced by A. using this scheme encrypt the text with the 
+      given shift and return the result to the client.*/
+  app.get('/cipher', (req, res) => {
+    const { text, shift } = req.query;
+  
+    // validation: both values are required, shift must be a number
+    if(!text) {
+      return res
+            .status(400)
+            .send('text is required');
+    }
+  
+    if(!shift) {
+      return res
+            .status(400)
+            .send('shift is required');
+    }
+  
+    const numShift = parseFloat(shift);
+  
+    if(Number.isNaN(numShift)) {
+      return res
+            .status(400)
+            .send('shift must be a number');
+    }
+  
+    // all valid, perform the task
+    // Make the text uppercase for convenience
+    // the question did not say what to do with punctuation marks
+    // and numbers so we will ignore them and only convert letters.
+    // Also just the 26 letters of the alphabet in typical use in the US
+    // and UK today. To support an international audience we will have to
+    // do more
+    // Create a loop over the characters, for each letter, covert
+    // using the shift
+  
+    const base = 'A'.charCodeAt(0);  // get char code 
+  
+    const cipher = text
+      .toUpperCase()
+      .split('') // create an array of characters
+      .map(char => { // map each original char to a converted char
+        const code = char.charCodeAt(0); //get the char code
+  
+        // if it is not one of the 26 letters ignore it
+        if(code < base || code > (base + 26)) {
+          return char;
+        }
+        
+        // otherwise convert it
+        // get the distance from A
+        let diff = code - base;
+        diff = diff + numShift; 
+        
+        // in case shift takes the value past Z, cycle back to the beginning
+        diff = diff % 26;
+  
+        // convert back to a character
+        const shiftedChar = String.fromCharCode(base + diff);
+        return shiftedChar;
+      })
+      .join(''); // construct a String from the array
+  
+    // Return the response
+    res
+      .status(200)
+      .send(cipher); 
+  });
+  
+  /* Drill 3: To send an array of values to the server via a query string simply 
+     repeat the key with different values. For instance, the query string 
+     ?arr=1&arr=2&arr=3 results in the query object { arr: [ '1', '2', '3' ] }.
+     Create a new endpoint /lotto that accepts an array of 6 distinct numbers 
+     between 1 and 20 named numbers. The function then randomly generates 6 numbers
+     between 1 and 20. Compare the numbers sent in the query with the randomly 
+     generated numbers to determine how many match. If fewer than 4 numbers match respond
+     with the string "Sorry, you lose". If 4 numbers match respond with the string 
+     "Congratulations, you win a free ticket", if 5 numbers match respond with 
+     "Congratulations! You win $100!". If all 6 numbers match respond with 
+     "Wow! Unbelievable! You could have won the mega millions!".*/
+  app.get('/lotto', (req, res) => {
+    const { numbers } = req.query; 
+  
+    // validation: 
+    // 1. the numbers array must exist
+    // 2. must be an array
+    // 3. must be 6 numbers
+    // 4. numbers must be between 1 and 20
+  
+    if(!numbers) {
+      return res
+        .status(200)
+        .send("numbers is required");
+    }
+  
+    if(!Array.isArray(numbers)) {
+      return res
+        .status(200)
+        .send("numbers must be an array");
+    }
+  
+    const guesses = numbers
+          .map(n => parseInt(n))
+          .filter(n => !Number.isNaN(n) && (n >= 1 && n <= 20));
+    
+    if(guesses.length != 6) {
+      return res
+        .status(400)
+        .send("numbers must contain 6 integers between 1 and 20");
+    }      
+  
+    // fully validated numbers
+  
+    // here are the 20 numbers to choose from
+    const stockNumbers = Array(20).fill(1).map((_, i) => i + 1);
+  
+    //randomly choose 6
+    const winningNumbers = [];
+    for(let i = 0; i < 6; i++) {
+      const ran = Math.floor(Math.random() * stockNumbers.length);
+      winningNumbers.push(stockNumbers[ran]);
+      stockNumbers.splice(ran, 1);
+    }
+  
+    //compare the guesses to the winning number
+    let diff = winningNumbers.filter(n => !guesses.includes(n));
+  
+    // construct a response
+    let responseText;
+  
+    switch(diff.length){
+      case 0: 
+        responseText = 'Wow! Unbelievable! You could have won the mega millions!';
+        break;
+      case 1:   
+        responseText = 'Congratulations! You win $100!';
+        break;
+      case 2:
+        responseText = 'Congratulations, you win a free ticket!';
+        break;
+      default:
+        responseText = 'Sorry, you lose';  
+    }
+  
+  
+    // uncomment below to see how the results ran
+  
+    // res.json({
+    //   guesses,
+    //   winningNumbers,
+    //   diff,
+    //   responseText
+    // });
+  
+    res.send(responseText);
+  });
+
+
+
 app.listen(8000, () => {
     console.log('Express server is listening on port 8000!');
 });
